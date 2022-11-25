f036c574da845dbb6781b61a993bea1b431d4612
kevinkorza <kevingkorza@gmail.com>
2022-11-25 17:55:57 -0500

    Portfolio page

--- my-app/src/Pages/myportfolio.js
+++ my-app/src/Pages/myportfolio.js
@@ -6,10 +6,24 @@ function MyPortfolio() {
         <h1>My Portfolio</h1>
         <h2>My Work</h2>
         <ul>
-            <li>
+            <li>    
             <a href="https://kevingkorza.github.io/WorkDay_Schedule/" target="_blank">Work Day Scheduler</a>
-    
             </li>
+            <br>
+            </br>
+            <img src={require('../../src/Images/workdayschedule.png')} height={400} width={600} />
+            <li>
+                <a href="https://kevingkorza.github.io/PasswordGenerator/" target="_blank">Password Generator</a>
+            </li>
+            <br>
+            </br>
+            <img src={require('../../src/Images/passwordgenerator.png')} height={400} width={600} />
+            <li>
+                <a href ="https://sheltered-atoll-76141.herokuapp.com/" target="_blank">Team Generator</a>
+            </li>
+            <br>
+            </br>
+            <img src={require('../../src/Images/teamgenerator.png')} height={400} width={600} />
         </ul>
         
 
