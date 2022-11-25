d5045629f075555eae8a678d4d4bc98998e521dc
kevinkorza <kevingkorza@gmail.com>
2022-11-25 13:14:24 -0500

    about page, still working on implelemting an image

--- my-app/src/Pages/aboutme.js
+++ my-app/src/Pages/aboutme.js
@@ -8,13 +8,15 @@
 
 import React from "react";
 
+
 function AboutMe() {
     return(
     <div>
-        <h1>About Page</h1>
-        <p>
-        Hello! 
-        </p>
+        <h1>About Me</h1>
+        <p>Hello! Welcome to my Portfolio! My name is Kevin Korza!</p>
+        <img src={require('../Images/me.jpg').default} />
+        <p>I am a graduate of The University of Connecticut where I have received my Bachelor's degree in Communication!</p> 
+        <p>My background includes Production, Media, Editing, Writing, Graphic Design, and Marketing!</p>
         </div>
     );
 }
