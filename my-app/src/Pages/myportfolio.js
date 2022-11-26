5ef2f997def6f28967a5bf6de6968a5f560ddb4e
kevinkorza <kevingkorza@gmail.com>
2022-11-26 17:21:00 -0500

    linking github repos to my portfolio page

--- my-app/src/Pages/myportfolio.js
+++ my-app/src/Pages/myportfolio.js
@@ -9,18 +9,21 @@ function MyPortfolio() {
             <li>    
             <a href="https://kevingkorza.github.io/WorkDay_Schedule/" target="_blank">Work Day Scheduler</a>
             </li>
+            <a href="https://github.com/KevinGKorza/WorkDay_Schedule" target="_blank">GitHub Repo</a>
             <br>
             </br>
             <img src={require('../../src/Images/workdayschedule.png')} height={400} width={600} />
             <li>
                 <a href="https://kevingkorza.github.io/PasswordGenerator/" target="_blank">Password Generator</a>
             </li>
+            <a href ="https://github.com/KevinGKorza/PasswordGenerator" target="_blank">GitHub Repo</a>
             <br>
             </br>
             <img src={require('../../src/Images/passwordgenerator.png')} height={400} width={600} />
             <li>
                 <a href ="https://sheltered-atoll-76141.herokuapp.com/" target="_blank">Team Generator</a>
             </li>
+            <a href ="https://github.com/garrettj94/DreamTeamManagement" target="_blank">GitHub Repo</a>
             <br>
             </br>
             <img src={require('../../src/Images/teamgenerator.png')} height={400} width={600} />
